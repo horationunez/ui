f233667e030464e71c2e864fe9ef4ccd8d5341fb
Matt Stover <matthews@kiva.org>
2018-07-10 13:30:23 -0700

    Simple login form on page-two, required manually copied crumb from actual kiva login form to work.

--- src/pages/PageTwo.vue
+++ src/pages/PageTwo.vue
@@ -5,6 +5,14 @@
 				<h1>Another Page</h1>
 				<p>This is page two!</p>
 				<p><router-link to="/">go home</router-link></p>
+
+				<form :action="actionUrl">
+					<input placeholder="Enter Email Address" type="email" name="email">
+					<input placeholder="Enter Email Address" type="password" name="password">
+					<button type="submit" name="loginForm_submit" id="loginForm_submit">Sign in</button>
+					<!-- <input type="hidden" name="currURL" value="https://dev-vm-01.kiva.org/page-two"> -->
+					<input type="hidden" id="crumb" name="crumb" value="caae1ede91">
+				</form>
 			</div>
 		</div>
 	</www-page>
@@ -19,6 +27,11 @@ export default {
 	inject: ['apollo'],
 	metaInfo: {
 		title: 'Page two'
+	},
+	data() {
+		return {
+			actionUrl: 'https://dev-vm-01.kiva.org/login/process?doneUrl=https%3A%2F%2Fdev-vm-01.kiva.org%2Fui-site-map'
+		};
 	},
 	apollo: {
 		query: helloQuery,
